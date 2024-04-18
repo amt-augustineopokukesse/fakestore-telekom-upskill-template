375ed47e8e43c1531a7296ab96e3a200c556c390
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-04-18 08:20:32 +0000

    feat: create endpoints for features

---
+++ src/app/core/constant/APIConstant.ts
@@ -0,0 +1,17 @@
+export const APIConstant = {
+//   BASE_URL: 'https://fakestoreapi.com/',
+//   CATEGORIES: 'products/categories',
+//   PRODUCTS: 'products',
+  login: 'auth/login',
+  user: {
+    createNewUser: 'users',
+    getAllUsers: 'users',
+    getSingleUser: 'users/:id',
+  },
+  products: {
+    getAllProducts: 'products',
+    getSingleProduct: 'products/:id',
+    getAllCategories: 'products/categories',
+    getProductsByCategory: 'products/category/:category',
+  }
+};
