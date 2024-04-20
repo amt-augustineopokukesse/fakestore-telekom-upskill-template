705889969e16a0e03348258833399c576efdfeb1
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-04-20 08:36:52 +0000

    feat: login 'should create' test feature

--- src/app/pages/auth/login/login.component.spec.ts
+++ src/app/pages/auth/login/login.component.spec.ts
@@ -1,15 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MasterService } from 'src/app/core/service/master/master.service';
+import { MatCardModule } from '@angular/material/card';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { of, throwError } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+  let mockMasterService: jasmine.SpyObj<MasterService>;
+  let mockRouter: jasmine.SpyObj<Router>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
+  beforeEach(async () => {
+    mockAuthService = jasmine.createSpyObj('AuthService', ['login']);
+    mockMasterService = jasmine.createSpyObj('MasterService', ['post']);
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, HttpClientTestingModule, MatCardModule, SharedModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [LoginComponent],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: MasterService, useValue: mockMasterService },
+        { provide: Router, useValue: mockRouter }
+      ]
+    }).compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
